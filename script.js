c4a0d2423f444a7bfaa1d4c8aff283d9a13d83c6
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-01-01 13:29:24 +0500

    Link Updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const skills = document.querySelector('.nav-links')
 
 const section = document.querySelector('.skill-section')
@@ -21,4 +22,29 @@ const header = document.querySelector('.header')
 
 about.addEventListener('click', function(){
     header.scrollIntoView({behavior: "smooth"});
+=======
+const skills = document.querySelector('.nav-links')
+
+const section = document.querySelector('.skill-section')
+
+const contact = document.querySelector('.contact-button')
+
+const info = document.querySelector('.footer-container')
+
+
+skills.addEventListener('click', function(e){
+    e.preventDefault();
+    section.scrollIntoView({behavior: "smooth"})
+})
+
+contact.addEventListener('click', function(){
+    info.scrollIntoView({behavior: 'smooth'});
+})
+
+const about = document.querySelector('.about-1')
+const header = document.querySelector('.header')
+
+about.addEventListener('click', function(){
+    header.scrollIntoView({behavior: "smooth"});
+>>>>>>> d2231ab (Link Updated)
 })
